9d70b676b24acdc5ae14f48d5abca8650b04aeec
Davee Sok <daveesok@gmail.com>
2021-09-12 11:40:03 -0500

    added readme

--- a_project/settings.py
+++ a_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-INSERT_NEW_SECRET_KEY_HERE"
+SECRET_KEY = "django-insecure-p^)45$jyy#h^n-$8izy&b^#csrs&bwv9b655rp$orfa89two=0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     "rest_framework",
     # Custom
     "a_users",
-    # Examples
-    "Example",
+    # Recipes
+    "recipe",
 ]
 
 MIDDLEWARE = [
@@ -81,8 +81,12 @@ WSGI_APPLICATION = "a_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
@@ -132,8 +136,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "a_users.CustomUser"
 
+# Were changing from AllowAny to IsAuthenticated
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.AllowAny",
+        "rest_framework.permissions.IsAuthenticated",
     ]
 }
--- recipe/admin.py
+++ recipe/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Example
+from .models import Recipe
 
 
-@admin.register(Example)
-class AdminForExample(admin.ModelAdmin):
+@admin.register(Recipe)
+class AdminForRecipe(admin.ModelAdmin):
     list_display = ("title", "author", "created_at")
     # prepopulated_fields = {
     #     "slug": ("title",),
